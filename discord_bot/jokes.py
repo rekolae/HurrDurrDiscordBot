1c5f0a22e921499b4f1217ee6bc559afe6a2101e
Emil Rekola <emil.rekola@hotmail.com>
2021-03-19 18:50:35 +0200

    [3-jokes] Added couple of joke categories

---
+++ discord_bot/jokes.py
@@ -0,0 +1,14 @@
+"""
+Contains selection of jokes in different categories and functions for serving them
+Emil Rekola <emil.rekola@hotmail.com>
+"""
+
+jokes = {
+    "Dad": [],
+    "Dark": [],
+    "Yo Mama": [],
+    "Sports": [],
+    "Blonde": [],
+    "Bar": [],
+    "Puns": []
+}
