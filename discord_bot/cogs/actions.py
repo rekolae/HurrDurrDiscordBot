e5441771f2250abe7f5350da6e800a7db4b176ef
Emil Rekola <emil.rekola@hotmail.com>
2021-03-31 20:26:40 +0300

    [8-flip-a-coin] Moved some commands to the more appropriate class

--- discord_bot/cogs/actions.py
+++ discord_bot/cogs/actions.py
@@ -3,12 +3,14 @@ Module containing Discord bot functionality
 Emil Rekola <emil.rekola@hotmail.com>
 """
 
+# STD imports
 from datetime import datetime
 
+# 3rd-party imports
 from discord.ext import commands
 
+# Local imports
 from discord_bot import VERSION, GITHUB_URL
-from discord_bot.utils import jokes
 from discord_bot.cogs.bot_mixin import BotMixin
 
 
@@ -16,9 +18,6 @@ class Actions(BotMixin, commands.Cog):
     """
     Bot actions
     """
-
-    seppuku_img_url = "https://i.pinimg.com/originals/b0/7b/70/b07b702182e46735c5925429db1a4492.jpg"
-    joke_categories = f"Supported categories: {jokes.get_categories()}"
 
     @commands.command()
     async def ping(self, ctx):
@@ -60,31 +59,6 @@ class Actions(BotMixin, commands.Cog):
 
         uptime = f"Bot uptime: {time_delta.days} days {hours} hours {mins} minutes {secs} seconds"
         await ctx.send(uptime)
-
-    @commands.command(aliases=["commitSeppuku"])
-    @commands.cooldown(rate=1, per=15.0, type=commands.BucketType.user)
-    async def seppuku(self, ctx):
-        """
-        Shamefur dispray
-        """
-
-        await ctx.send(self.seppuku_img_url)
-
-    @commands.command(
-        brief="Tell a random joke",
-        description="Tell a random joke from a category if specified",
-        help=joke_categories
-    )
-    async def joke(self, ctx,  *, category=None):
-        """
-        Tell a random joke
-        """
-
-        # Try to remove double/single quotes from the given category
-        if category is not None:
-            category = category.replace("'", "").replace('"', "")
-
-        await ctx.send(jokes.get_random_joke_from_category(category))
 
 
 def setup(bot):
--- discord_bot/cogs/helpers.py
+++ discord_bot/cogs/helpers.py
@@ -12,12 +12,16 @@ from discord.ext import commands
 
 # Local imports
 from discord_bot.cogs.bot_mixin import BotMixin
+from discord_bot.utils import jokes
 
 
 class Helpers(BotMixin, commands.Cog):
     """
     Helper bot actions
     """
+
+    seppuku_img_url = "https://i.pinimg.com/originals/b0/7b/70/b07b702182e46735c5925429db1a4492.jpg"
+    joke_categories = f"Supported categories: {jokes.get_categories()}"
 
     @commands.command(aliases=["coinToss"])
     async def flip(self, ctx):
@@ -36,6 +40,31 @@ class Helpers(BotMixin, commands.Cog):
 
         else:
             await ctx.send("Tails!")
+
+    @commands.command(aliases=["commitSeppuku"])
+    @commands.cooldown(rate=1, per=15.0, type=commands.BucketType.user)
+    async def seppuku(self, ctx):
+        """
+        Shamefur dispray
+        """
+
+        await ctx.send(self.seppuku_img_url)
+
+    @commands.command(
+        brief="Tell a random joke",
+        description="Tell a random joke from a category if specified",
+        help=joke_categories
+    )
+    async def joke(self, ctx, *, category=None):
+        """
+        Tell a random joke
+        """
+
+        # Try to remove double/single quotes from the given category
+        if category is not None:
+            category = category.replace("'", "").replace('"', "")
+
+        await ctx.send(jokes.get_random_joke_from_category(category))
 
 
 def setup(bot):
