e3d6d08b7a52756d3699863e189eb9baeba90134
Emil Rekola <emil.rekola@hotmail.com>
2021-03-31 22:35:38 +0300

    [10-team-randomizer] Implemented command for deciding teams from people

--- discord_bot/cogs/helpers.py
+++ discord_bot/cogs/helpers.py
@@ -5,7 +5,8 @@ Emil Rekola <emil.rekola@hotmail.com>
 
 # STD imports
 from asyncio import sleep
-from random import random
+from random import random, choice
+from itertools import zip_longest
 
 # 3rd-party imports
 from discord.ext import commands
@@ -65,6 +66,69 @@ class Helpers(BotMixin, commands.Cog):
             category = category.replace("'", "").replace('"', "")
 
         await ctx.send(jokes.get_random_joke_from_category(category))
+
+    @commands.command(aliases=["makeTeams"])
+    async def team(self, ctx, even_teams: bool, *people):
+        """
+        Create teams, first arg signifies if teams should be even
+        """
+
+        if len(people) == 0:
+            await ctx.send("No people to choose from!")
+
+        elif len(people) == 1:
+            await ctx.send(f"'{people[0]}' has no friends and has to play alone \U0001F625")
+
+        # Decide teams when there is not even amount of people and even teams were requested
+        elif even_teams and (len(people) % 2 != 0):
+            removed_person = choice(people)
+            await ctx.send(f"Teams are not be even, so somebody has to be left out and that one is '{removed_person}'")
+
+            people_left = [p for p in people if p != removed_person]
+
+            team1 = []
+            team2 = []
+
+            # Pick one person, remove from list and add to one of the teams
+            for x in range(len(people_left)):
+                p = choice(people_left)
+                people_left.remove(p)
+                if x % 2 == 0:
+                    team1.append(p)
+
+                else:
+                    team2.append(p)
+
+            teams = "Teams have been decided:"
+            teams += f"\n{'Team1:'.ljust(20)}Team2:"
+            for x, y in zip(team1, team2):
+                teams += f"\n{x.ljust(20)} {y}"
+
+            await ctx.send(teams)
+
+        # Randomize teams without caring about balance
+        else:
+            team1 = []
+            team2 = []
+
+            people_list = [p for p in people]
+
+            # Pick one person, remove from list and add to one of the teams
+            for x in range(len(people)):
+                p = choice(people_list)
+                people_list.remove(p)
+                if x % 2 == 0:
+                    team1.append(p)
+
+                else:
+                    team2.append(p)
+
+            teams = "Teams have been decided:"
+            teams += f"\n{'Team1:'.ljust(20)}Team2:"
+            for x, y in zip_longest(team1, team2, fillvalue=""):
+                teams += f"\n{x.ljust(20)} {y}"
+
+            await ctx.send(teams)
 
 
 def setup(bot):
